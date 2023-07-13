3cde7986d3bab39ada643876cb74050a44c3316a
djamiranda <djamiranda@gmail.com>
2023-07-13 03:17:26 -0400

    added server.js & routes dir with html & api files

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require('./routes/api')(app);
+require('./routes/html')(app);
+
+app.listen(PORT, () => {
+  console.log(`Server available at localhost${PORT} 🚀`);
+});
